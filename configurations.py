6a5317aa4d664d63190de3ed9c8652f258e51562
Tal Dugma <154086680+TalDugma@users.noreply.github.com>
2024-09-24 11:57:40 +0300

    Update configurations.py

--- configurations.py
+++ configurations.py
@@ -1,8 +1,9 @@
 DATA_GENERATOR = {
-    "num_scenes" : 200,  # Number of scenes
+    "num_scenes" : 2,  # Number of scenes
     "num_images_per_scene" : 15,  # Number of images per scene
-    "tweezers_object_folder_path": "surgical_tools_models/tweezers",  # Path to the folder containing the tweezers objects
-    "needle_holder_object_folder_path": "surgical_tools_models/needle_holder",  # Path to the folder containing the needle holder objects
+    "tweezers_object_folder_path": "/datashare/project/surgical_tools_models/tweezers",  # Path to the folder containing the tweezers objects
+    "needle_holder_object_folder_path": "/datashare/project/surgical_tools_models/needle_holder",  # Path to the folder containing the needle holder objects
     "motion_blur_ratio": 0.7,  # 0.5 means 50% of the images (and scenes) will have motion blur
-    "image_dataset_folder_name": "coco_data",  # Name of the folder where the images will be saved (fixed typo)
+    "image_dataset_folder_name": "coco_data",  # Name of the folder where the images will be saved
+    "haven_path": "/datashare/project/haven/" # Path to folder with HDRI files
 }
